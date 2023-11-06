039c032e08cf565db4eafe3aceaf02e105f5e33f
BoBo185 <githuthalydia@gmail>
2023-11-06 09:59:02 +0300

    lesson

--- Week-1/variables.py
+++ Week-1/variables.py
@@ -8,6 +8,6 @@
 #Variables
 f_number =20
 s_number =50
-print("The first number is:", f_number)
+print("The first number is:",f_number)
 print("The second number is:",s_number)
 print("")
---
+++ Week-8/data science.py
@@ -0,0 +1,2 @@
+#data science-it is the process of using data to find solutions for a problem statement
+#use of figma to design the portfolio website
